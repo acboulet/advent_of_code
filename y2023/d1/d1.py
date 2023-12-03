fb938447e0ea0ee9154e8207f988dd28bfc68dd2
acboulet <39838871+acboulet@users.noreply.github.com>
2023-12-03 23:14:34 +0000

    working on v2

--- y2023/d1/d1.py
+++ y2023/d1/d1.py
@@ -7,21 +7,51 @@ import sys
 ## Look at parent directory for imports
 sys.path.append('../')
 
-
-import os
-print(os.getcwd())
 from advent2023 import PerLineImport
     
 class CalibrationValue(PerLineImport):
+    def __init__(self, datafile) -> None:
+        super().__init__(datafile)
+
+        self.calib_sum = 0
+
     def process_lines(self, line):
-        print(line.strip() * 2)
+        ## Combine first and last digit
+        ## Track sum
+        first = self.find_first_digit(line.strip())
+        last = self.find_last_digit(line.strip())
+
+        self.calib_sum += int(f'{first}{last}')
     
+    def find_first_digit(self, calib_string):
+        for char in calib_string:
+            if char.isdigit():
+                return char
+        return None
+    
+    def find_last_digit(self, calib_string):
+        for char in reversed(calib_string):
+            if char.isdigit():
+                return char
+        return None
+
+    def main_v1(self):
+        self.import_file()
+        print(self.calib_sum)
+
+    def main_v2(self):
+        self.import_file()
+        # print(self.calib_sum)
+
 
 if __name__ == "__main__":
     file = 'test.txt'
-
-    # advent = AdventImport(file)
-    # advent.import_file()
+    file = 'data.txt'
 
     calib = CalibrationValue(file)
-    calib.import_file()
+    # calib.import_file()
+    calib.main_v1()
+    ## answer 54632
+
+    calib.main_v2()
+    
